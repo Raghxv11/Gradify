cb3094b38f6cf4082e6e0bf15bc45bbdd2a16339
aroramrinaal <apply.mrinaal@gmail.com>
2024-10-29 21:55:53 -0700

    heroku setup

--- server.py
+++ server.py
@@ -293,4 +293,5 @@ def visualization_pdf():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
